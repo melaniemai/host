9e5a391349796fad0eeb1623b22b84df3135af9a
melaniemai <44130793+melaniemai@users.noreply.github.com>
2021-04-19 19:17:54 -0400

    Update host.py

--- host.py
+++ host.py
@@ -1,105 +1,43 @@
 import socket
-import ipaddress
-import platform
-import struct
+import sys
 
 
-# get hostname 
-# hostname = socket.gethostname()
-# print("Host Name for this machine : " + hostname)
+def getHostName():
+    hostname = socket.gethostname()
+    print("Host Name for this machine : " + hostname)
 
-# # find IP4 address(s)
-# ip4add = socket.gethostbyname_ex(hostname)[-1]
-# print("IPv4 Address Assigned to this machine are :")
-# for i in ip4add:
-#      print("    " + i)
+def getIP():
+    # --------------------- get IPv4 ----------------------
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    k = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# # find IP6 address(s)
-# print("IPv6 Address Assigned to this machine are : ")
-# prefix = "::ffff:"
-# for _ in ip4add:
-#      print("    " + prefix + _)
+    try:
+        ipList = []
+        # doesn't even have to be reachable
+        s.connect(('8.8.8.8', 80))
+        k.connect(('10.255.255.255', 1))
+        if(s.getsockname()[0] == k.getsockname()[0]):
+            ipList.append(s.getsockname()[0])
+        else:
+            ipList.append(s.getsockname()[0])
+            ipList.append(k.getsockname()[0])
+    except:
+        ipList = '127.0.0.1'
+    finally:
+        s.close()
 
-# def get_ip():
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     try:
-#         # doesn't even have to be reachable
-#         s.connect(('10.255.255.255', 1))
-#         IP = s.getsockname()[0]
-#     except:
-#         IP = '127.0.0.1'
-#     finally:
-#         s.close()
-#     return IP
+    print("IPv4 Address Assigned to this machine are :")
+    for i in ipList:
+        print("\t " + i)
 
-typeOfOS = int(input("Press '1' if OS is Unix/Mac\nPress '2' for Windows: "))
-
-if(typeOfOS == 1):
-     import fcntl
-     def getHostName():
-          hostname = socket.gethostname()
-          print("Host Name for this machine : " + hostname)
-
-     def getIPv4and6():
-          address = socket.gethostbyname(socket.gethostname())
-          s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-          try:
-               # doesn't even have to be reachable
-               s.connect(('10.255.255.255', 1))
-               IP = s.getsockname()
-          except:
-               IP = '127.0.0.1'
-          finally:
-               s.close()
-     
-          print("IPv4 Address Assigned to this machine are :")
-          print("    " + address)
-          if(len(IP) != 0 and IP[0] != address):
-               print("    " + IP[0])
+    # --------------------- get IPv6 ----------------------
+    print("IPv6 Address Assigned to this machine are :")
+    j = 0
+    while j < len(ipList):
+        ipadd = socket.getaddrinfo(ipList[j], None, socket.AF_INET6)[0][4][0]
+        print("\t " + ipadd)
+        j += 1
 
 
-          ipadd = socket.getaddrinfo(address, None, socket.AF_INET6)[0][4][0]
-          print("IPv6 Address Assigned to this machine are :")
-          print("    " + ipadd)
-          if(len(IP) != 0 and IP[0] != address):
-               print("    " + socket.getaddrinfo(IP[0], None, socket.AF_INET6)[0][4][0])
-
-
-          # print(socket.gethostbyname_ex(socket.gethostname())[-1])
-
-          getHostName()
-          getIPv4and6()
-
-# def getHostName():
-#      hostname = socket.gethostname()
-#      print("Host Name for this machine : " + hostname)
-
-# def getIPv4and6():
-#      address = socket.gethostbyname(socket.gethostname())
-#      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#      try:
-#         # doesn't even have to be reachable
-#         s.connect(('10.255.255.255', 1))
-#         IP = s.getsockname()
-#      except:
-#         IP = '127.0.0.1'
-#      finally:
-#         s.close()
-     
-#      print("IPv4 Address Assigned to this machine are :")
-#      print("    " + address)
-#      if(len(IP) != 0 and IP[0] != address):
-#           print("    " + IP[0])
-
-
-#      ipadd = socket.getaddrinfo(address, None, socket.AF_INET6)[0][4][0]
-#      print("IPv6 Address Assigned to this machine are :")
-#      print("    " + ipadd)
-#      if(len(IP) != 0 and IP[0] != address):
-#           print("    " + socket.getaddrinfo(IP[0], None, socket.AF_INET6)[0][4][0])
-
-
-# # print(socket.gethostbyname_ex(socket.gethostname())[-1])
-
-# getHostName()
-# getIPv4and6()
+getHostName()
+getIP()
